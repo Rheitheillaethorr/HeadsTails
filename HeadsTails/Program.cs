973ba58282896418ddfdeb9bb2ac0b391baf2cf0
Rheitheillaethor <minegocu3@gmail.com>
2021-10-13 16:48:47 +0200

    Backup, conditions are done; based on Read Key from keyboard, coin flip based on random number 0 or 1 probably should have use bool but "Next()" method returns int

--- HeadsTails/Program.cs
+++ HeadsTails/Program.cs
@@ -6,7 +6,23 @@ namespace HeadsTails
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
+        static void Rand()
+        {
+            Random rnd = new Random();
+            int RandomNumber = rnd.Next(0, 1);
+            Console.WriteLine("Press 'Enter' to flip a coin or 'q' for quit!");
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine("draw");
+            }
+            else if (Console.ReadKey().Key == ConsoleKey.Q) { }
+            else
+            {
+                Console.WriteLine("You choosed wrong letter, try again");
+            }
+        }
+
     }
 }
